49d71e4b3064d5826a7b2ece2927ff697455ee81
Adya <62937747+adyaaaa@users.noreply.github.com>
2022-03-31 11:34:33 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Home';
+import Login from './Login';
+import Dashboard from './Dashboard';
+import Chatbot from './Chatbot';
+import { Route, Routes, BrowserRouter as Router, Switch,  Link } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Button from './Components/Button';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar></Navbar>
+      <Header></Header>
+      <Home> </Home>
+
+      <Link to = "/">Home</Link><br></br>
+      <Link to = "/login">Login</Link><br></br>
+      <Link to = "/dashboard">Dashboard</Link><br></br>
+      <Link to = "/chatbot">Chatbot</Link><br></br>
+    
+      
+    </>
   );
 }
 
