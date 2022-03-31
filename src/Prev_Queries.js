96a0025d2aad4cb2677fd77d837ed167af2ab15e
Adya <62937747+adyaaaa@users.noreply.github.com>
2022-03-31 16:37:20 +0100

    Add files via upload

--- src/Prev_Queries.js
+++ src/Prev_Queries.js
@@ -1,16 +1,39 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import './style.css'
 
 function Prev_Queries(){
     return(
         <div>
             <h1>Previous Queries</h1>
+            <table id="prev_queries_tbl">
+                <tr>
+                    <th>Type</th>
+                    <th>Date/Time</th>
+                    <th>Query</th>
+                    <th>Solved</th>
+                    <th>Solved by</th>
+                    <th>Rating</th>
+                </tr>
 
-            <h2>ADD EXAMPLE TEXT HERE</h2>
+                <tr>
+                    <td>General</td>
+                    <td>15-03-2022 15:20</td>
+                    <td>Need help with editing personal details</td>
+                    <td>Yes</td>
+                    <td>Adya Aggarwal</td>
+                    <td>5/5</td>
+                </tr>
 
-            <Link to = "/">Home</Link><br></br>
-            <Link to = "/chatbot">Chatbot</Link><br></br>
-      
+                <tr>
+                    <td>Technical</td>
+                    <td>01-02-2022 11:40</td>
+                    <td>Can not share screen</td>
+                    <td>Yes</td>
+                    <td>Adya Aggarwal</td>
+                    <td>4/5</td>
+                </tr>
+
+            </table>
         </div>
     )
 }
