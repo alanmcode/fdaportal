1532579903fe50e7a453544596b5e5f92d8166fb
alanmcode <68718613+alanmcode@users.noreply.github.com>
2022-04-06 01:17:15 +0100

    Add files via upload

---
+++ src/Components/Chatbot/Chatbot.js
@@ -0,0 +1,29 @@
+import React from "react";
+import './Chatbot.css'
+import {Link} from "react-router-dom"
+
+const Form = () => {
+    return (
+      <div className='blogContainer'>
+          <form className='formBox'>
+              <h1 style={{textAlign:"center", color:"white"}}>QUERY</h1>
+              <section className='everythingInBox'>
+                  <div className='twoBoxes'>
+                      <div>
+                          <input className='subject' type="text" placeholder='Subject' />
+                      </div>
+                      <div>
+                          <textarea className='queryBox' placeholder='Please input your query here..'/>
+                      </div>
+                  </div>
+                  <div className='bothBtns'>
+                      <button className='submitBtn' style={{color:"white"}} type="submit" value="POST">SUBMIT</button>
+                      <button className='cancelBtn' style={{color:"white"}} type="reset">CANCEL</button>
+                  </div>
+              </section>
+          </form>
+      </div>
+    )
+  }
+  
+  export default Form
